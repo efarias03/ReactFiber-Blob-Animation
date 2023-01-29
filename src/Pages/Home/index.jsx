0ac5c89dd2b8e9c2bfc4c73a788c01750ff685ad
Enzo Farias <enzofarias030603@gmail.com>
2023-01-29 16:14:44 -0300

    first commit

---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,14 @@
+import { Canvas } from "@react-three/fiber";
+import { Blob } from "../../components/Blob";
+
+export function Home() {
+
+    return (
+        <div className="container">
+            <Canvas camera={{ position: [0.0, 0.0, 8.0] }}>
+                <Blob />
+            </Canvas>
+            <h1>Blob animation React Fiber</h1>
+        </div>
+    )
+}
